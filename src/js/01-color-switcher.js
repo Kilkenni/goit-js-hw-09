5b544c1755ce2070c920d19900440ec11c794d4e
Kilkenni <hpittman89@outlook.com>
2021-11-28 19:21:06 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartElem = document.querySelector("button[data-start]");
+const btnStopElem = document.querySelector("button[data-stop]");
+
+btnStartElem.addEventListener("click", startBkgRainbow);
+
+btnStopElem.addEventListener("click", stopBkgRainbow);
+
+let bkgTimerID = 0;
+
+function startBkgRainbow(event) {
+    btnStartElem.disabled = true;
+    //console.log("starting Rainbow Factory");   
+    bkgTimerID = setInterval(recolorBkg, 1000);
+}
+
+function recolorBkg() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopBkgRainbow(event) {
+    try {
+        clearInterval(bkgTimerID);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    btnStartElem.disabled = false;
+}
